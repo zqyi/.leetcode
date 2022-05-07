b6aa74ff7db3ca215430ba572c98aecd9e01ba6d
Your Name <you@example.com>
2022-05-07 20:38:26 +0800

    modify

---
+++ 628.三个数的最大乘积.cpp
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=628 lang=cpp
+ *
+ * [628] 三个数的最大乘积
+ */
+
+// @lc code=start
+
+#include <vector>
+using namespace;
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+
+    }
+};
+// @lc code=end
+
